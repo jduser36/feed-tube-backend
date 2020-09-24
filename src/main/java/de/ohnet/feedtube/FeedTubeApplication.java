c93e62504ba0e524585e6673b1f89559a7b4b016
Daniel Beyer <duser@danbey.de>
2020-09-24 15:23:00 +0200

    initial source commit

---
+++ src/main/java/de/ohnet/feedtube/FeedTubeApplication.java
@@ -0,0 +1,13 @@
+package de.ohnet.feedtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedTubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ohnet/feedtube/FeedTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ohnet.feedtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
